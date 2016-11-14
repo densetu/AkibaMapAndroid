a1076532768ead30f0811faa210eab0bbd65be7e
tomex <android.tomex@gmail.com>
2016-11-14 17:18:06 +0900

    commit.

---
+++ app/src/main/java/net/dentare/akibamapandroid/activity/BaseSubActivity.java
@@ -0,0 +1,10 @@
+package net.dentare.akibamapandroid.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tmx3 on 2016/10/24.
+ */
+
+public class BaseSubActivity extends AppCompatActivity {
+}
